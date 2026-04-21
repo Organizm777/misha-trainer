d4c40e3cb33889a36c825e95b748e30dd28c9711
Organizm777 <pvolosov@gmail.com>
2026-04-21 13:44:27 +0300

    perf: display:flow-root on .w to stop 12px margin-collapse shift

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'trainer-build-wave86h';
+const CACHE_NAME = 'trainer-build-wave86i';
 const STATIC_CACHE = CACHE_NAME + '-static';
 const RUNTIME_CACHE = CACHE_NAME + '-runtime';
 const ASSETS = [
