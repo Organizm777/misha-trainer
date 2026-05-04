b33c83aebf722031fa90d583c22541e20a44e2d0
Organizm777 <pvolosov@gmail.com>
2026-05-04 12:02:04 +0300

    clean

--- assets/js/inline_diagnostic_2_wave86u.1bca35f107.js
+++
@@ -1,1 +0,0 @@
-(function(){try{var qs=new URLSearchParams(location.search||'');if(qs.get('lhci')==='1'||navigator.webdriver)return}catch(_err){}if('serviceWorker' in navigator){navigator.serviceWorker.register('./sw.js',{updateViaCache:'none'}).catch(()=>{})}})();
